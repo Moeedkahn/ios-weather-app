9e70e20272b20f490c950a778fbe4338cde2418e
Moeed Khan <moeedkahn@gmail.com>
2026-04-03 23:15:21 +0500

    Codable structs that map exactly to the Open-Meteo API response added

--- WeatherApp/Models/WeatherData.swift
+++ WeatherApp/Models/WeatherData.swift
@@ -5,3 +5,54 @@
 //  Created by Moeed Khan on 02/04/2026.
 //
 
+import Foundation
+
+// MARK: - Root response
+struct WeatherResponse: Codable {
+    let current:  CurrentWeather
+    let hourly:   HourlyWeather
+    let daily:    DailyWeather
+}
+
+// MARK: - Current weather
+struct CurrentWeather: Codable {
+    let temperature:  Double
+    let weatherCode:  Int
+    let windSpeed:    Double
+    let humidity:     Double
+
+    enum CodingKeys: String, CodingKey {
+        case temperature  = "temperature_2m"
+        case weatherCode  = "weathercode"
+        case windSpeed    = "windspeed_10m"
+        case humidity     = "relativehumidity_2m"
+    }
+}
+
+// MARK: - Hourly forecast
+struct HourlyWeather: Codable {
+    let time:         [String]
+    let temperature:  [Double]
+    let weatherCode:  [Int]
+
+    enum CodingKeys: String, CodingKey {
+        case time
+        case temperature  = "temperature_2m"
+        case weatherCode  = "weathercode"
+    }
+}
+
+// MARK: - Daily forecast
+struct DailyWeather: Codable {
+    let time:           [String]
+    let weatherCode:    [Int]
+    let maxTemperature: [Double]
+    let minTemperature: [Double]
+
+    enum CodingKeys: String, CodingKey {
+        case time
+        case weatherCode    = "weathercode"
+        case maxTemperature = "temperature_2m_max"
+        case minTemperature = "temperature_2m_min"
+    }
+}
