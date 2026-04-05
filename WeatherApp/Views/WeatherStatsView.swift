87dbb0d6fe83b900b99d6307b5f87bfb97e33796
Moeed Khan <moeedkahn@gmail.com>
2026-04-05 18:25:49 +0500

    WeatherView added

--- WeatherApp/Views/WeatherStatsView.swift
+++ WeatherApp/Views/WeatherStatsView.swift
@@ -5,3 +5,28 @@
 //  Created by Moeed Khan on 02/04/2026.
 //
 
+import SwiftUI
+
+struct WeatherStatsView: View {
+    let windSpeed: String
+    let humidity:  String
+    let condition: WeatherCondition
+
+    var body: some View {
+        HStack(spacing: 12) {
+            StatCardView(
+                icon:      "wind",
+                label:     "Wind",
+                value:     windSpeed,
+                condition: condition
+            )
+
+            StatCardView(
+                icon:      "humidity.fill",
+                label:     "Humidity",
+                value:     humidity,
+                condition: condition
+            )
+        }
+    }
+}
